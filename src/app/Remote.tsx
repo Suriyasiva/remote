7d2015fd423d8ab571f9dde05cac1af588095af0
suriya <suriya.s@buildingblocks.la>
2025-03-10 20:38:15 +0530

    fix routing

--- src/app/Remote.tsx
+++ src/app/Remote.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import TempUser from './pages/TempUser';
+import User from './pages/User';
 
 
 
 function Remote() {
   return (
     <Routes>
-      <Route path='/user/:userId' element={<TempUser/>} />
+      <Route path='/user/:userId' element={<User/>} />
+      <Route path='/' element={<TempUser/>} />
     </Routes>
   );
 }
